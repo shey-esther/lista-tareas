3f2e27d8d4bbb4543880a4473c22df0d5277843d
shey-esther <estheralvaro44@gmail.com>
2017-07-25 19:08:25 -0700

    recorriendo el array-objeto

---
+++ js/main.js
@@ -0,0 +1,82 @@
+var arrayTareas =[ {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }];
+
+
+  var html = "";
+
+  for (var i = 0; i < arrayTareas.length; i++) {
+    var tarea1 = arrayTareas[i].title;
+    var titulos = "<div>"+"<li>" + tarea1+ "</li>" +"</div>";
+
+    html += titulos;
+  }
+  document.getElementById("mostrar").innerHTML = html;
+
+function añadir(){
+
+    var agregar = document.getElementById("s").value;
+    document.getElementById("tareas").innerHTML+= "<li>" + agregar + "</li>"
+}
+
+
+
+
+
